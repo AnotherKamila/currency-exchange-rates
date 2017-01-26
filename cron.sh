ed58b244b9ba69f30cd21523b1c25a025447c32f
Kamila Souckova <kamila@ksp.sk>
2017-01-26 09:44:57 +0000

    add push to cron script

--- cron.sh
+++ cron.sh
@@ -2,5 +2,7 @@
 
 cd "$(dirname "$0")"
 . venv/bin/activate
-./getrates.py rates.csv
-git commit -m "[automatic commit] rates for $(date +%Y-%m-%d)" -o -- rates.csv
+export LANG=en_US.UTF-8
+./getrates.py rates.csv >> cronlog.txt 2>>cronerr.txt
+git commit -m "[automatic commit] rates for $(date +%Y-%m-%d)" -o -- rates.csv >> cronlog.txt 2>>cronerr.txt
+git push >> cronlog.txt 2>> cronerr.txt
